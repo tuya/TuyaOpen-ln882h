f2c15c5aeae22363a1f6899bddf39c357ab4da64
KyleDeng <dlkxiaok@163.com>
2025-05-11 12:06:47 +0800

    support params file

--- build_example.sh
+++ build_example.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/sh
-# $1 - example name: echo_app_top
-# $2 - example version: 1.0.0
-# $3 - header files directory:
-# $4 - libs directory:
-# $5 - libs: tuyaos tuyaapp
-# $6 - output directory:
+# 参数说明：
+# $1 - params path: echo_app_top
+# $2 - user cmd: build / clean / ...
 
 print_not_null()
 {
@@ -19,13 +16,28 @@ print_not_null()
 set -e
 cd `dirname $0`
 
-EXAMPLE_NAME=$1         # the example name selected when ./configure
-EXAMPLE_VER=$2          # the version, default is 1.0.0
-HEADER_DIR=$3           # the include file directory, default is build/include
-LIBS_DIR=$4             # the libraries file directory, default is build/lib
-LIBS=$5                 # the libraries file, default is libtuyaapp & libtuyaos
-OUTPUT_DIR=$6           # the output file directory, default is build/bin
-USER_CMD=$7             # the user compile command
+BUILD_PARAM_DIR=$1
+BUILD_PARAM_FILE=$BUILD_PARAM_DIR/build_param.config
+. $BUILD_PARAM_FILE
+
+EXAMPLE_NAME=$CONFIG_PROJECT_NAME         # the example name selected when ./configure
+EXAMPLE_VER=$CONFIG_PROJECT_VERSION          # the version, default is 1.0.0
+HEADER_DIR=$OPEN_HEADER_DIR           # the include file directory, default is build/include
+LIBS_DIR=$OPEN_LIBS_DIR             # the libraries file directory, default is build/lib
+LIBS=$PLATFORM_NEED_LIBS                 # the libraries file, default is libtuyaapp & libtuyaos
+OUTPUT_DIR=$BIN_OUTPUT_DIR           # the output file directory, default is build/bin
+USER_CMD=$2             # the user compile command
+BOARD_NAME=$PLATFORM_BOARD
+
+# echo EXAMPLE_NAME=$EXAMPLE_NAME
+# echo EXAMPLE_VER=$EXAMPLE_VER
+# echo HEADER_DIR=$HEADER_DIR
+# echo LIBS_DIR=$LIBS_DIR
+# echo LIBS=$LIBS
+# echo OUTPUT_DIR=$OUTPUT_DIR
+# echo USER_CMD=$USER_CMD
+# echo BOARD_NAME=$BOARD_NAME
+# exit 0
 
 TOP_DIR=$(pwd)
 
